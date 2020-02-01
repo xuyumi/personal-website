84f622b2ea411c0972bdbb6bdc379ac6aead85fc
xuyumi <isabelle.xu88@gmail.com>
2020-02-01 16:52:00 -0500

    Updates

---
+++ precache-manifest.24c0038121d482e7bacfad74239027f8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55c3e5f300ff094ec1f9f6910ec9adc",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "f3810888b73a58109fb1",
+    "url": "/Personal-Website/static/css/main.4ff1f20a.chunk.css"
+  },
+  {
+    "revision": "af1ca44aa3f273963eb2",
+    "url": "/Personal-Website/static/js/2.0dd5d5de.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Personal-Website/static/js/2.0dd5d5de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3810888b73a58109fb1",
+    "url": "/Personal-Website/static/js/main.d36a1032.chunk.js"
+  },
+  {
+    "revision": "e8e49e076e175017f05b",
+    "url": "/Personal-Website/static/js/runtime-main.d0e00159.js"
+  },
+  {
+    "revision": "d1cbb369b7229015193715030d119c45",
+    "url": "/Personal-Website/static/media/About.d1cbb369.png"
+  },
+  {
+    "revision": "6b6d9433782da867376f0e53b8228f53",
+    "url": "/Personal-Website/static/media/Banner.6b6d9433.jpg"
+  },
+  {
+    "revision": "5b7a9ba0705517c01adbd3bc301e36f1",
+    "url": "/Personal-Website/static/media/Logo.5b7a9ba0.png"
+  },
+  {
+    "revision": "bfbc9a6c9635355b92424eeb6cdc3533",
+    "url": "/Personal-Website/static/media/Qevi.bfbc9a6c.png"
+  },
+  {
+    "revision": "4829b33228e731c16ccc3b2b991aab1d",
+    "url": "/Personal-Website/static/media/ResumeIXU.4829b332.pdf"
+  },
+  {
+    "revision": "bf5912621bbbc88bc8850374a33f9ebe",
+    "url": "/Personal-Website/static/media/lighthouse.bf591262.PNG"
+  },
+  {
+    "revision": "e03b483d6730b99f7b4833b838303468",
+    "url": "/Personal-Website/static/media/photon.e03b483d.jpg"
+  }
+]);
